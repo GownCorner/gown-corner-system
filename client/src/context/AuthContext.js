00363e47082bd29f54f13dbdcaa8957c3a3e4806
GownCorner <gdlcapstone@gmail.com>
2024-12-09 13:48:38 +0800

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -3,6 +3,8 @@ import axios from "axios";
 
 export const AuthContext = createContext();
 
+const url = "https://gown-booking-system.onrender.com/api"; // Base backend URL
+
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
@@ -18,12 +20,9 @@ export const AuthProvider = ({ children }) => {
             return;
           }
 
-          const response = await axios.get(
-            "https://gown-booking-system.onrender.com/api/auth/me", // Updated Render backend URL
-            {
-              headers: { Authorization: `Bearer ${token}` },
-            }
-          );
+          const response = await axios.get(`${url}/auth/me`, {
+            headers: { Authorization: `Bearer ${token}` },
+          });
           setUser(response.data);
         } catch (error) {
           console.error("Error fetching user:", error.message);
@@ -37,13 +36,10 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-      const response = await axios.post(
-        "https://gown-booking-system.onrender.com/api/auth/login", // Updated Render backend URL
-        {
-          email,
-          password,
-        }
-      );
+      const response = await axios.post(`${url}/auth/login`, {
+        email,
+        password,
+      });
       localStorage.setItem("token", response.data.token); // Save token
       setUser(response.data.user); // Update user state immediately
     } catch (error) {
